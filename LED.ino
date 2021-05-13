c39e4ffe1ab70044151cd738a9c1305d44b9c9b0
İlker Koçer <55910508+ilker410@users.noreply.github.com>
2021-05-13 14:04:44 +0300

    Add files via upload

---
+++ LED.ino
@@ -0,0 +1,14 @@
+const int LED=9; //9. pini led olarak tanımlıyoruz.
+                 //Bu zorunlu değildir sadece projeyi yaparken kolaylık sağlar bu kısımı silip aşağıda LED yazan yerlere 9 yazsak da kod çalışır.
+
+
+void setup() {
+ pinMode(LED, OUTPUT); //Led i yani 9. pini çıkış olarak ayarlıyoruz.
+digitalWrite(LED, HIGH); //Led e Yani 9. pine enerji veriyoruz.
+}
+
+void loop() {
+
+  //Void loop kısımını boş bırakıyoruz.
+
+}
